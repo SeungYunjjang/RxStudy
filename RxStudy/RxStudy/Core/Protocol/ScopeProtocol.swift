dec209ecc295c61845bef8e40c50e38e02f1b2ee
차상운 <chachabebop@gmail.com>
2019-12-30 22:31:04 +0900

    <CHOR> build new project

---
+++ RxStudy/RxStudy/Core/Protocol/ScopeProtocol.swift
@@ -0,0 +1,36 @@
+//
+//  ScopeProtocol.swift
+//  RxStudy
+//
+//  Created by Cha Cha on 30/12/2019.
+//  Copyright © 2019 Cha corp. All rights reserved.
+//
+
+import Foundation
+
+protocol HasLet {}
+protocol HasApply {}
+
+extension HasLet {
+    func lets(closure: (Self) -> Void) {
+        closure(self)
+    }
+}
+
+extension HasApply {
+    func apply(closure: (Self) -> Void) -> Self {
+        closure(self)
+        return self
+    }
+}
+
+extension NSObject: HasApply, HasLet { }
+
+
+func run<R>(closure: () -> R) -> R {
+    return closure()
+}
+
+func with<T, R>(_ receiver: T, closure: (T) -> R) -> R {
+    return closure(receiver)
+}
---
+++ RxStudy/RxStudy/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RxStudy
+//
+//  Created by Cha Cha on 30/12/2019.
+//  Copyright © 2019 Cha corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
